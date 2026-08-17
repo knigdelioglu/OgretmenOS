2b3bb25665b5238ea3fcb634803a64adf634cabb
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-17 17:04:22 +0300

    Add persistent outcome tracking service

--- lib/app/app_dependencies.dart
+++ lib/app/app_dependencies.dart
@@ -4,38 +4,59 @@ import '../data/calendar/asset_weekly_planning_service.dart';
 import '../data/course/course_database_installer.dart';
 import '../data/course/course_knowledge_repository_impl.dart';
 import '../data/preferences/user_preferences_repository.dart';
+import '../data/tracking/outcome_tracking_database.dart';
 import '../domain/models/weekly_plan_models.dart';
 import '../domain/repositories/course_knowledge_repository.dart';
+import '../domain/services/outcome_planning_service.dart';
 
 class AppDependencies {
   const AppDependencies({
     required this.repository,
     required this.preferences,
     required this.weeklyPlanning,
+    this.outcomePlanning,
     this.dispose,
   });
 
   final CourseKnowledgeRepository repository;
   final UserPreferencesRepository preferences;
   final WeeklyPlanningService weeklyPlanning;
+  final OutcomePlanningService? outcomePlanning;
   final Future<void> Function()? dispose;
 }
 
 Future<AppDependencies> loadProductionDependencies() async {
   final database = await CourseDatabase.open();
+  OutcomeTrackingDatabase? trackingDatabase;
   try {
     final preferences = await SharedPreferences.getInstance();
     final repository = CourseKnowledgeRepositoryImpl(
       dataSource: database.dataSource,
       manifest: database.manifest,
     );
+    final weeklyPlanning = AssetWeeklyPlanningService(repository: repository);
+    trackingDatabase = await OutcomeTrackingDatabase.open();
+    final trackingRepository = SqfliteOutcomeTrackingRepository(
+      trackingDatabase.database,
+    );
+    final outcomePlanning = OutcomePlanningService(
+      repository: repository,
+      weeklyPlanning: weeklyPlanning,
+      trackingRepository: trackingRepository,
+    );
+
     return AppDependencies(
       repository: repository,
       preferences: SharedPreferencesUserPreferences(preferences),
-      weeklyPlanning: AssetWeeklyPlanningService(repository: repository),
-      dispose: database.close,
+      weeklyPlanning: weeklyPlanning,
+      outcomePlanning: outcomePlanning,
+      dispose: () async {
+        await trackingDatabase?.close();
+        await database.close();
+      },
     );
   } on Object {
+    await trackingDatabase?.close();
     await database.close();
     rethrow;
   }
